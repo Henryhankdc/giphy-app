72cbfe17853971cb296946d94e2b5435eaa5899e
Henry Mesias <henrymesias@gmail.com>
2017-12-31 14:23:43 -0500

    started working on adding gif based on input text and button functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,6 +14,9 @@ function starterButtons(){
 }
 starterButtons();
 
+function general() {
+
+// On click for gif buttons.
     $('.gif-button').on('click', function(e){
 
         e.preventDefault();
@@ -65,3 +68,84 @@ starterButtons();
 
     });
 //   end onclick
+
+}
+general();
+
+// function for adding new gif group and buttons
+
+function newGifButton(){
+
+    $('#submit-btn').on('click', function(e){
+        e.preventDefault();
+        var inputValue = $('#gif-text').val();
+    console.log(inputValue);
+        
+    // create a new button with data value
+    var newButton =$('<button type="button" class="btn btn-secondary gif-button">');
+        $('#gif-button-wrapper').append(newButton);
+        $(newButton).attr('data-value', inputValue);
+        $(newButton).text(inputValue);
+
+        // HERE***********************************
+        // On click for gif buttons.
+    $('.gif-button').on('click', function(e){
+
+        e.preventDefault();
+        $("#gif-holder").empty();
+
+        console.log('clicked');
+        var gifValue = $(this).attr("data-value");
+
+
+         // Constructing a URL to search Giphy for the name of the person who said the quote
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    gifValue + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+  // Performing our AJAX GET request
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+
+     // After the data comes back from the API
+     .done(function(response) {
+         console.log(response);
+        // Storing an array of results in the results variable
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            // create div to hold gif image
+            var gifItemDiv = $("<div class='item'>");
+
+            // create gif img tag
+            var gifImage = $("<img>");
+
+            // Giving the image tag an src attribute of a proprty pulled off the
+            // result item
+            gifImage.attr("src", results[i].images.fixed_height_small.url);
+
+              // Appending the paragraph and personImage we created to the "gifDiv" div we created
+            // gifDiv.append(p);
+            gifItemDiv.append(gifImage);
+
+            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+            $("#gif-holder").prepend(gifItemDiv);
+
+        }
+
+
+     });
+
+
+    });
+
+        // ****************************************
+
+    });
+
+
+
+
+}
+newGifButton();
